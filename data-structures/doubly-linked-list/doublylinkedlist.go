4b557ad9f39e849e251c160d09bb989092f7d151
Enrico Galassi <enrico.galassi.88@gmail.com>
2019-12-19 01:18:28 +0100

    Add addTail function for doubly-linked-list

--- data-structures/doubly-linked-list/doublylinkedlist.go
+++ data-structures/doubly-linked-list/doublylinkedlist.go
@@ -15,6 +15,31 @@ func newDoublyLinkedListNode(value int) *DoublyLinkedListNode {
 	return &DoublyLinkedListNode{value, nil, nil}
 }
 
+func (node *DoublyLinkedListNode) addHead(newNode *DoublyLinkedListNode) *DoublyLinkedListNode {
+	if newNode == nil {
+		return node
+	}
+
+	newNode.next = node
+	node.previous = newNode
+	return newNode
+}
+
+func (node *DoublyLinkedListNode) addTail(newNode *DoublyLinkedListNode) *DoublyLinkedListNode {
+	if newNode == nil {
+		return node
+	}
+
+	if node.next == nil {
+		node.next = newNode
+		newNode.previous = node
+	} else {
+		node.next.addTail(newNode)
+	}
+
+	return node
+}
+
 func (node *DoublyLinkedListNode) toString() string {
 	var sb strings.Builder
 
@@ -24,8 +49,13 @@ func (node *DoublyLinkedListNode) toString() string {
 			break
 		}
 
+		prevValue := "nil"
+		if currentNode.previous != nil {
+			prevValue = strconv.Itoa(currentNode.previous.value)
+		}
+		sb.WriteString(prevValue + "<~")
 		sb.WriteString(strconv.Itoa(currentNode.value))
-		sb.WriteString(" <-> ")
+		sb.WriteString(" -- ")
 		currentNode = currentNode.next
 	}
 
--- data-structures/doubly-linked-list/doublylinkedlist_test.go
+++ data-structures/doubly-linked-list/doublylinkedlist_test.go
@@ -16,15 +16,86 @@ func TestNewDoublyLinkedListNode(t *testing.T) {
 		t.Errorf("NewDoublyLinkedListNode was incorrect, for next got: %v, want nil.", actual.next)
 	}
 }
+func TestAddHeadWithNull(t *testing.T) {
+	dll := newDoublyLinkedListNode(3)
+
+	result := dll.addHead(nil)
+
+	expected := "nil<~3 -- EODLL"
+	actual := result.toString()
+	if actual != expected {
+		t.Errorf("DoublyLinkedList addHead with nil was incorrect, got: %s, want: %s.", actual, expected)
+	}
+}
+
+func TestAddHead(t *testing.T) {
+	cases := []struct {
+		dll        *DoublyLinkedListNode
+		valueToAdd []int
+		expected   string
+	}{
+		{newDoublyLinkedListNode(7), []int{3}, "nil<~3 -- 3<~7 -- EODLL"},
+		{newDoublyLinkedListNode(10), []int{25, 41, 90}, "nil<~90 -- 90<~41 -- 41<~25 -- 25<~10 -- EODLL"},
+		{newDoublyLinkedListNode(3), []int{4, 35}, "nil<~35 -- 35<~4 -- 4<~3 -- EODLL"},
+	}
+
+	for _, c := range cases {
+		result := c.dll
+		for _, v := range c.valueToAdd {
+			result = result.addHead(newDoublyLinkedListNode(v))
+		}
+		actual := result.toString()
+
+		if actual != c.expected {
+			t.Errorf("DoublyLinkedList addHead was incorrect, got: %s, want: %s.", actual, c.expected)
+		}
+	}
+}
+
+func TestAddTailWithNull(t *testing.T) {
+	dll := newDoublyLinkedListNode(3)
+
+	result := dll.addTail(nil)
+
+	expected := "nil<~3 -- EODLL"
+	actual := result.toString()
+	if actual != expected {
+		t.Errorf("DoublyLinkedList addTail with nil was incorrect, got: %s, want: %s.", actual, expected)
+	}
+}
+
+func TestAddTail(t *testing.T) {
+	cases := []struct {
+		ll         *DoublyLinkedListNode
+		valueToAdd []int
+		expected   string
+	}{
+		{newDoublyLinkedListNode(7), []int{3}, "nil<~7 -- 7<~3 -- EODLL"},
+		{newDoublyLinkedListNode(10), []int{25, 41, 90}, "nil<~10 -- 10<~25 -- 25<~41 -- 41<~90 -- EODLL"},
+		{newDoublyLinkedListNode(3), []int{4, 35}, "nil<~3 -- 3<~4 -- 4<~35 -- EODLL"},
+	}
+
+	for _, c := range cases {
+		result := c.ll
+		for _, v := range c.valueToAdd {
+			result = result.addTail(newDoublyLinkedListNode(v))
+		}
+		actual := result.toString()
+
+		if actual != c.expected {
+			t.Errorf("DoublyLinkedList addTail was incorrect, got: %s, want: %s.", actual, c.expected)
+		}
+	}
+}
 
 func TestToString(t *testing.T) {
 	cases := []struct {
-		dll      DoublyLinkedListNode
+		dll      *DoublyLinkedListNode
 		expected string
 	}{
-		{DoublyLinkedListNode{3, nil, nil}, "3 <-> EODLL"},
-		{DoublyLinkedListNode{17, nil, &DoublyLinkedListNode{31, nil, &DoublyLinkedListNode{35, nil, nil}}}, "17 <-> 31 <-> 35 <-> EODLL"},
-		{DoublyLinkedListNode{3, nil, &DoublyLinkedListNode{4, nil, nil}}, "3 <-> 4 <-> EODLL"},
+		{newDoublyLinkedListNode(3), "nil<~3 -- EODLL"},
+		{newDoublyLinkedListNode(17).addTail(newDoublyLinkedListNode(31)).addTail(newDoublyLinkedListNode(35)), "nil<~17 -- 17<~31 -- 31<~35 -- EODLL"},
+		{newDoublyLinkedListNode(3).addTail(newDoublyLinkedListNode(4)), "nil<~3 -- 3<~4 -- EODLL"},
 	}
 
 	for _, c := range cases {
